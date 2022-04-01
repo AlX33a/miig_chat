cafec8b522f5947d5594c75c204dfbd2582d23f9
ArtemMikhashin <91016586+ArtemMikhashin@users.noreply.github.com>
2022-04-01 22:13:53 +0300

    new br

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+body{
+  background-color: rgb(111, 111, 233);
+  margin: 0;
+  font-family:Arial, Helvetica, sans-serif;
+}
+
+</style>
